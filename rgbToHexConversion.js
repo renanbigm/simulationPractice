746267f4591089792d94d396745d7c901b5fa514
Renan Fagundes <renanbigm@gmail.com>
2023-10-14 12:09:13 -0300

    restart challenge

--- rgbToHexConversion.js
+++ rgbToHexConversion.js
@@ -1,23 +1,5 @@
 function rgbToHexConversion(r, g, b) {
-    let hexR = decimalToHex(r);
-    let hexG = decimalToHex(g);
-    let hexB = decimalToHex(b);
-
-    return hexR + hexG + hexB;
-}
-
-function decimalToHex(decimal) {
-    const hexDecimals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-    let hexString = "";
-    
-    while (decimal !== 0) {
-        let remainder = decimal % 16;
-        decimal = Math.floor(decimal / 16);
-        hexString = hexDecimals[remainder] + hexString;
-    }
-    
-    return hexString.padStart(2, '0');
+    console.log(r,g,b);
 }
 
 module.exports = { rgbToHexConversion };
